31b345ef2743e6085a8938eb0fa944c50960d7e2
ayesha2524 <as8034684@gmail.com>
2025-05-29 22:06:21 +0500

    Add files via upload

---
+++ src/components/css/header.js
@@ -0,0 +1,6 @@
+export const header = {
+    hWrapp: 'bg-black text-slate-300',
+    hContainer: 'flex justify-between p-8 items-center w-[100%] gap-8',
+    hMenu: 'flex justify-center items-center flex-wrap gap-9 hover:cursor-pointer',
+    hButton: 'py-2 px-6 bg-sky-500 font-medium text-white border-none rounded-md transition-all ease-in duration-200 hover:scale-110 cursor-pointer'
+}
---
+++ src/components/css/hero.js
@@ -0,0 +1,14 @@
+export const hero={
+     heroWrapp: ' text-white relative pb-8 bg-black',
+     heroContainer:  'flex justify-center items-center gap-8 flex-wrap w-[100%] p-6 ',
+     heroLeft: 'flex flex-col gap-8',
+     orangeCircle: 'w-16 h-16 bg-orange-500 rounded-full absolute right-[59%]',
+     search: 'w-[80%] bg-white p-2 flex justify-center items-center gap-3 border-[2px] border-[rgb(135,206,235)] rounded-md flex-wrap',
+     button: 'py-2 px-6 bg-sky-500 font-medium text-white border-none rounded-md transition-all ease-in duration-200 hover:scale-105 cursor-pointer',
+     imagrContainer: 'w-[25rem] h-[30rem] rounded-tl-[15rem] rounded-tr-[15rem] rounded-br-[0] rounded-bl-[0] border-2 border-[rgba(255,255,255,0.12)] overflow-hidden',
+     heroDesc: 'flex flex-col justify-center items-start text-slate-300',
+     heroNum: 'flex justify-start items-center gap-8 flex-wrap w-[100%] p-6',
+     Nums: 'flex flex-col justify-center items-start gap-1 align-center',
+
+
+}
