4a44891301bc41d2d4b4845cba9f23a93952a475
ayesha2524 <as8034684@gmail.com>
2025-05-29 22:09:44 +0500

    Add files via upload

---
+++ src/utills/Slides.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image1 from '../assets/r1.png'
+import Image2 from '../assets/r2.png'
+import Image3 from '../assets/r3.png'
+export  const data = [
+        {
+            "price": "47,043",
+            "name": "Aliva Priva Jardin",
+            "details": "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+            "img": Image1
+
+        },
+        {
+            "price": "66,353",
+            "name": "Asatti Garden City",
+            "details": "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Bara",
+            "img": Image2
+
+        },
+        {
+            "price": "35,853",
+            "name": "Citralan Puri Serang",
+            "details": "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+            "img": Image3
+
+        },
+                {
+            "price": "47,043",
+            "name": "Aliva Priva Jardin",
+            "details": "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+            "img": Image1
+
+        },
+        {
+            "price": "66,353",
+            "name": "Asatti Garden City",
+            "details": "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Bara",
+            "img": Image2
+
+        },
+
+    ]
