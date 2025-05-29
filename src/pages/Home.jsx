8ada73bfe82cda578352ca4141183fb0e466ff70
ayesha2524 <as8034684@gmail.com>
2025-05-29 22:09:05 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Companies from '../components/Companies'
+import Sliders from '../components/Sliders'
+import Value from '../components/Value'
+import Contact from '../components/Contact'
+import Getstarted from '../components/Getstarted'
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Companies />
+      <Sliders />
+      <Value />
+      <Contact />
+      <Getstarted />
+    </div>
+  )
+}
+
+export default Home
